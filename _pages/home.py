76b852e6fd3b76bc424f39df45341ff0e9e5a90f
Ayushman Anupam <165472365+AyushmanGHub@users.noreply.github.com>
2025-06-12 17:53:27 +0530

    Update home.py

--- _pages/home.py
+++ _pages/home.py
@@ -116,7 +116,7 @@ st.markdown("""
         padding: 12px !important;
         font-weight: 500;
         font-size: 16px;
-        color: #aaaaaa !important;
+        color: #000000 !important;
         box-shadow: none !important;
     }
 
