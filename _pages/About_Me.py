31efbf0bedb2a3cb4673ce1f12d34a9a831225fb
Ayushman Anupam <165472365+AyushmanGHub@users.noreply.github.com>
2025-08-17 14:24:34 +0530

    Update About_Me.py

--- _pages/About_Me.py
+++ _pages/About_Me.py
@@ -115,7 +115,7 @@ st.markdown("""
 <div style="font-size: 18px; text-align: justify;">
 Hi, I'm <b>Ayushman Anupam</b>, a Data Science student who loves working with data, solving real-world problems, and continuously exploring how mathematics, statistics, and AI can be used to make meaningful impact.
 
-I come from a strong Mathematics background, which helped me develop deep interests in <b>Machine Learning, Graph Theory, Statistical Modeling, and Agentic AI</b>. I'm particularly excited about building intelligent systems that turn data into actionable insights.
+Coming from a Mathematics background has helped me develop deep interests in <b>Machine Learning, Graph Theory, Statistical Modeling, and Agentic AI</b>. I'm particularly excited about building intelligent systems that turn data into actionable insights.
 
-Currently, I am exploring the world of finance, specifically the role of time series analysis in financial markets, equity, and related domains. ALong, with it I am Working on a Time-Series project to automates real-time stock market analysis anf forecasting some Indian Stocks and Indexes by integrating data fetching, feature engineering, predictive modeling, and visualization.</div>
+Currently, I am exploring the world of finance, specifically the role of time series analysis in financial markets, equity, and related domains. ALong, with it I am Working on a Time-Series project to automates real-time stock market analysis and forecasting some Indian Stocks and Indexes by integrating data fetching, feature engineering, predictive modeling, and visualization.</div>
 """, unsafe_allow_html=True)
